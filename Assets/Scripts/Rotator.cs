39c8513bb600ac4a49792d35c8cf3982f25b6e4b
Whalex <hsc19930818@gmail.com>
2018-10-13 18:56:27 -0400

    add a rotator script

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	
+	float delta;
+	
+	public Quaternion rotation = Quaternion.identity;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		delta += 1 * Time.deltaTime;
+		//transform.rotation = Quaternion.Euler(0, delta, 0);
+		rotation.eulerAngles = new Vector3(0, delta, 0);
+		transform.rotation = rotation;
+		//print(transform.rotation.eulerAngles.y);
+	}
+}
