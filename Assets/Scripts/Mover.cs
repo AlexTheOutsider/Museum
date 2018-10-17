e98fe4a383972af62d36d2efc68fbfc3eaec0efa
Whalex <hsc19930818@gmail.com>
2018-10-16 22:33:50 -0400

    add DOTween, post-processing package and a function scene

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+	public int times;
+	// Use this for initialization
+	void Start ()
+	{
+		transform.DOMoveX(10, 5).SetEase(Ease.Linear)/*.onComplete(WhenDone())*/;
+	}
+
+	void WhenDone()
+	{
+		times++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
