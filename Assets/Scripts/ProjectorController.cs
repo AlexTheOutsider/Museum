7300b9b1a21e6a3fbe01d2e4fb58a8e170d657f0
Whalex <hsc19930818@gmail.com>
2018-11-06 01:22:58 -0500

    adjust music and model

--- Assets/Scripts/ProjectorController.cs
+++ Assets/Scripts/ProjectorController.cs
@@ -6,24 +6,34 @@ public class ProjectorController : MonoBehaviour
 {
 	public int activatedNum = 0;
 	public InteractiveDoor door;
+	public MusicController musicController;
+	public AudioClip music;
+	public AudioClip sound;
+	
+	private bool isActivated = false;
 
 	void Update ()
 	{
-		if (activatedNum == 4)
+		if (activatedNum == 4 && !isActivated)
 		{
 			StartCoroutine(StartProject());
 			door.isLocked = false;
+			isActivated = true;
 		}
 	}
 
 	IEnumerator StartProject()
 	{
-		GetComponent<Light>().enabled = true;
+		transform.GetChild(0).GetComponent<Light>().enabled = true;
+		musicController.GetComponent<AudioSource>().loop = false;
+		musicController.playMusic(sound);
 		yield return new WaitForSeconds(2);
 		
-		GetComponent<Light>().enabled = false;
+		transform.GetChild(0).GetComponent<Light>().enabled = false;
 		GetComponent<Projector>().enabled = true;
 		GetComponent<Rotator>().enabled = true;
+		musicController.GetComponent<AudioSource>().loop = true;
+		musicController.playMusic(music);
 	}
 
 	public void ActivateSwitch()
