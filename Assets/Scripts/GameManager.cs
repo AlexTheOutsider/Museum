c4f197a9787b7a2c2c935b82f37043d5819e5314
Whalex <hsc19930818@gmail.com>
2019-01-14 22:31:31 -0500

    a rough welcome menu, fixed door animation bug

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class GameManager : MonoBehaviour
+{
+    private Transform playerController;
+    private Camera mainCam;
+    private Transform mainMenu;
+    private Camera menuCam;
+
+    private void Start()
+    {
+        playerController = GameObject.Find("FPSController").transform;
+        mainCam = playerController.GetChild(0).GetComponent<Camera>();
+        mainMenu = GameObject.Find("MainMenu").transform;
+        menuCam = GameObject.Find("MenuCamera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            mainMenu.gameObject.SetActive(false);
+            menuCam.enabled = false;
+            playerController.GetComponent<FirstPersonController>().enabled = true;
+            mainCam.enabled = true;
+        }
+    }
+}
